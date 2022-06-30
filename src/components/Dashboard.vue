f3e4d6b0be8ffc0627a8b7b2408fe268aae82555
Louise Huang <louise_hy_huang@wistron.com>
2022-06-30 09:13:39 +0000

    feat(footer): add footer

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,14 +2,17 @@
   <div>
     <Navbar></Navbar>
     <router-view></router-view>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar.vue'
+import Footer from './Footer.vue'
 export default {
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 }
 </script>
