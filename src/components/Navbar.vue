730a2b28fb247166973591b33ccea625cfc70138
Louise Huang <louise_hy_huang@wistron.com>
2022-06-30 01:50:50 +0000

    feat(navbar): add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar navbar-light fixed-top shadow">
+    <div class="container">
+      <a class="navbar-brand mr-0" href="#">Toy Story</a>
+      <ul class="navbar-nav px-3 d-flex flex-row">
+        <li class="nav-item mr-4">
+          <a class="nav-link" href="#"><i class="fas fa-store-alt"></i>&ensp;Product</a>
+        </li>
+        <li class="nav-item mr-4">
+          <a class="nav-link" href="#"><i class="fas fa-user"></i>&ensp;Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i>&ensp;Cart</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+.navbar-brand {
+  color: #A93B20;
+}
+.navbar {
+  background-color: #EBEBE7;
+}
+</style>
